38112967b204f23a787e98619ca9100001f578e8
Ashley Haskins <a.haskins24@gmail.com>
2023-02-18 10:19:26 -0600

    working on 4b

--- scripts.sql
+++ scripts.sql
@@ -54,6 +54,7 @@ LIMIT 1;
 
 3. 
     a. Which drug (generic_name) had the highest total drug cost?
+	
 	SELECT d.generic_name, COUNT(pn.total_drug_cost)
 	FROM drug AS d
 	LEFT JOIN prescription AS pn
@@ -81,15 +82,13 @@ LIMIT 1;
 4. 
     a. For each drug in the drug table, return the drug name and then a column named 'drug_type' which says 'opioid' for drugs which have opioid_drug_flag = 'Y', says 'antibiotic' for those drugs which have antibiotic_drug_flag = 'Y', and says 'neither' for all other drugs.
 
-SELECT drug_name
+SELECT drug_name,
 CASE WHEN opioid_drug_flag = 'Y' THEN 'opioid'
-    WHEN antibiotic_drug_flag = 'Y' THEN 'antibiotic'
-    ELSE 'neither' END AS drug_type
-	FROM drug
-	GROUP BY drug_name
+WHEN antibiotic_drug_flag = 'Y' THEN 'antibiotic'
+ELSE 'neither'
+END AS drug_type
+FROM drug;
 
-select * 
-from drug
 
     b. Building off of the query you wrote for part a, determine whether more was spent (total_drug_cost) on opioids or on antibiotics. Hint: Format the total costs as MONEY for easier comparision.
 
